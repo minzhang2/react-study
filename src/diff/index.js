629f9e886c9634040c091e8eca3cca91271dcf99
minzhang2 <1018644425@qq.com>
2018-11-24 14:51:50 +0800

    add diff

--- src/diff/index.js
+++ src/diff/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 
 /**
  * diff
- * 1.不同组件则执行旧组件的卸载和新组件的挂载
- * 2.keys变更react的逻辑
+ * 1.tree diff：采用启发式算范，只对树的同一层级进行比对，避免o(n^3)的性能消耗
+ * 2.component diff：不同组件则执行旧组件的卸载和新组件的挂载
+ * 3.element diff：通过遍历nextChildren对比新旧节点，
+ *                 当prevChild._mountIndex < lastIndex则进行节点的移动,
+ *                 当prevChild._mountIndex = lastIndex则跳过当前节点,lastIndex++
+ *                 当prevChild._mountIndex > lastIndex则跳过当前节点；lastIndex = Math.max(prevChild._mountIndex, lastIndex)
+ *
  * 参考：https://zhuanlan.zhihu.com/p/20346379
  */
 
